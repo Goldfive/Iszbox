0a9a1c4e8f4286b14a1696a05c0b4ecc01c63fa9
Goldfive <jens.vanderwateren@gmail.com>
2015-02-28 17:19:47 +0100

    Initialized D3D11, set up the core and added background colors to show it works.

--- Iszbox/engine.cc
+++ Iszbox/engine.cc
@@ -1,12 +1,31 @@
 #include "engine.h"
 
-#include <Windows.h>
 
 LPCTSTR WndClassName = "firstwindow";
 HWND hwnd = NULL;
 
 const int width = 800;
 const int height = 600;
+
+//Globals
+
+IDXGISwapChain* swapChain;
+ID3D11Device* d3d11Device;
+ID3D11DeviceContext* d3d11DevCon;
+ID3D11RenderTargetView* renderTargetView;
+
+float red = 0.0f;
+float green = 0.0f;
+float blue = 0.0f;
+int colormodr = 1;
+int colormodg = 1;
+int colormodb = 1;
+
+bool InitializeDirect3d11App(HINSTANCE hInstance);
+void ReleaseObjects();
+bool InitScene();
+void UpdateScene();
+void DrawScene();
 
 bool InitializeWindow(HINSTANCE hInstance, int ShowWnd, int width, int height, bool windowed);
 
@@ -18,11 +37,24 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 {
 	if (!InitializeWindow(hInstance, nShowCmd, width, height, true))
 	{
-		MessageBox(0, "WIndow Initialization - Failed", "Error", MB_OK);
+		MessageBox(0, "Wndow Initialization - Failed", "Error", MB_OK);
+		return 0;
+	}
+
+	if (!InitializeDirect3d11App(hInstance)) //Initialize Direct3D
+	{
+		MessageBox(0, "Direct3D Initialization - Failed", "Error", MB_OK);
+		return 0;
+	}
+
+	if (!InitScene()) //Initialize our scene
+	{
+		MessageBox(0, "Scene Initialization - Failed", "Error", MB_OK);
 		return 0;
 	}
 
 	MessageLoop();
+	ReleaseObjects();
 
 	return 0;
 }
@@ -83,11 +115,105 @@ int MessageLoop()
 		else
 		{
 			//To-do game-code
+
+			UpdateScene();
+			DrawScene();
 		}
 	}
 	return msg.wParam;
 }
 
+
+//D3D functions
+
+bool InitializeDirect3d11App(HINSTANCE hInstance)
+{
+	HRESULT hr;
+
+	//Describe our Buffer
+	DXGI_MODE_DESC bufferDesc;
+	
+	ZeroMemory(&bufferDesc, sizeof(DXGI_MODE_DESC));
+
+	bufferDesc.Width = width;
+	bufferDesc.Height = height;
+	bufferDesc.RefreshRate.Numerator = 60;
+	bufferDesc.RefreshRate.Denominator = 1;
+	bufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	bufferDesc.ScanlineOrdering - DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	bufferDesc.Scaling - DXGI_MODE_SCALING_UNSPECIFIED;
+
+	//Describe our swapChain
+	DXGI_SWAP_CHAIN_DESC swapChainDesc;
+
+	ZeroMemory(&swapChainDesc, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+	swapChainDesc.BufferDesc = bufferDesc;
+	swapChainDesc.SampleDesc.Count = 1;
+	swapChainDesc.SampleDesc.Quality = 0;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.BufferCount = 1;
+	swapChainDesc.OutputWindow = hwnd;
+	swapChainDesc.Windowed = TRUE;
+	swapChainDesc.SwapEffect - DXGI_SWAP_EFFECT_DISCARD;
+
+	//Create our swapChain
+	hr = D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, NULL, NULL, NULL, D3D11_SDK_VERSION, &swapChainDesc, &swapChain, &d3d11Device, NULL, &d3d11DevCon);
+
+	//Create our BackBuffer
+	ID3D11Texture2D* BackBuffer;
+	hr = swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&BackBuffer);
+
+	//Create our Render Target
+	hr = d3d11Device->CreateRenderTargetView(BackBuffer, NULL, &renderTargetView);
+	BackBuffer->Release();
+
+	//Set our Render Target
+	d3d11DevCon->OMSetRenderTargets(1, &renderTargetView, NULL);
+
+	return true;
+}
+
+void ReleaseObjects()
+{
+	//Release the COM Objects we created
+	swapChain->Release();
+	d3d11Device->Release();
+	d3d11DevCon->Release();
+}
+
+bool InitScene()
+{
+	return true;
+}
+
+void UpdateScene()
+{
+	//Update the colors of our scene
+	red += colormodr * 0.00005f;
+	green += colormodg * 0.00002f;
+	blue += colormodb * 0.00001f;
+
+	if (red >= 1.0f || red <= 0.0f)
+		colormodr *= -1;
+	if (green >= 1.0f || green <= 0.0f)
+		colormodg *= -1;
+	if (blue >= 1.0f || blue <= 0.0f)
+		colormodb *= -1;
+}
+
+void DrawScene()
+{
+	//Clear our backbuffer to the updated color
+	D3DXCOLOR bgColor(red, green, blue, 1.0f);
+
+	d3d11DevCon->ClearRenderTargetView(renderTargetView, bgColor);
+
+	//Present the backbuffer to the screen
+	swapChain->Present(0, 0);
+}
+
+//Callback
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg)
